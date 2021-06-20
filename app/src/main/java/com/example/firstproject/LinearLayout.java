16b972a0185460f940dc473b8fb2972bf6b4cfeb
Rojen Maharjan <44132059+rojen11@users.noreply.github.com>
2021-06-20 10:07:23 +0545

    Classwork of second class.

--- app/src/main/java/com/example/firstproject/LinearLayout.java
+++ app/src/main/java/com/example/firstproject/LinearLayout.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LinearLayout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_relative_layout);
+        setContentView(R.layout.activity_linear_layout);
     }
 }
