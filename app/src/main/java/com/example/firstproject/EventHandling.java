a94f777f511f59c4aaabb5ba525aec9ba6a13222
Rojen Maharjan <44132059+rojen11@users.noreply.github.com>
2021-06-27 08:05:07 +0545

    Classwork of third class

---
+++ app/src/main/java/com/example/firstproject/EventHandling.java
@@ -0,0 +1,47 @@
+package com.example.firstproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class EventHandling extends AppCompatActivity {
+
+    private TextInputEditText input1;
+    private TextInputEditText input2;
+    private TextView output;
+    private Button submitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_handling);
+
+        input1 = findViewById(R.id.inputText1);
+        input2 = findViewById(R.id.inputText2);
+        output = findViewById(R.id.outputText);
+        submitButton = findViewById(R.id.submitButton);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Editable i1 = input1.getText();
+                Editable i2 = input2.getText();
+
+                int sum = Integer.parseInt(i1.toString()) + Integer.parseInt(i2.toString());
+                output.setText(String.valueOf(sum));
+
+                Toast.makeText(EventHandling.this, "Sum: " + String.valueOf(sum), Toast.LENGTH_LONG).show();
+
+
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/firstproject/MainActivity.java
+++ app/src/main/java/com/example/firstproject/MainActivity.java
@@ -2,13 +2,65 @@ package com.example.firstproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_table_layout);
+        setContentView(R.layout.activity_main);
+
+        Button relativeButton = findViewById(R.id.relativeButton);
+        Button linearButton = findViewById(R.id.linearButton);
+        Button frameButton = findViewById(R.id.frameButton);
+        Button tableButton = findViewById(R.id.tableButton);
+        Button eventButton = findViewById(R.id.eventButton);
+
+        relativeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RelativeLayout.class);
+                startActivity(intent);
+            }
+        });
+
+        linearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LinearLayout.class);
+                startActivity(intent);
+            }
+        });
+
+        frameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FrameLayout.class);
+                startActivity(intent);
+            }
+        });
+
+        tableButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TableLayout.class);
+                startActivity(intent);
+            }
+        });
+
+        eventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EventHandling.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
