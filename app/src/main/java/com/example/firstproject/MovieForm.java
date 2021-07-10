3888d13bd19e23911ac18968832092883b608038
Rojen Maharjan <44132059+rojen11@users.noreply.github.com>
2021-07-10 08:08:38 +0545

    Second assignment WIP.

---
+++ app/src/main/java/com/example/firstproject/MovieForm.java
@@ -0,0 +1,36 @@
+package com.example.firstproject;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MovieForm extends Fragment {
+
+    private MovieFormViewModel mViewModel;
+
+    public static MovieForm newInstance() {
+        return new MovieForm();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.movie_form_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(MovieFormViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/firstproject/MovieFormViewModel.java
@@ -0,0 +1,7 @@
+package com.example.firstproject;
+
+import androidx.lifecycle.ViewModel;
+
+public class MovieFormViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
